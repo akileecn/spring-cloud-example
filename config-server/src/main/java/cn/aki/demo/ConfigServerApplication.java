0b71a689e816823f6af115c62a2eced2c971e5b3
Akilee <akilee@qq.com>
2017-06-21 15:16:03 +0800

    动态路由 config

---
+++ config-server/src/main/java/cn/aki/demo/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package cn.aki.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by Administrator on 2017/6/20.
+ */
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+public class ConfigServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}
